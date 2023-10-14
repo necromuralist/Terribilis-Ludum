0b5164845c81415272106fc351de741ea86a0e49
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-10-14 17:18:10 -0500

    Fixed docker reference to bravo user

--- dockerfiles/pypy-official/run_image.fish
+++ dockerfiles/pypy-official/run_image.fish
@@ -2,5 +2,5 @@
 docker run -p 2228:22 --name terribilis-pypy \
        --mount type=bind,source=$HOME/projects/Terribilis-Ludum,target=/home/terribilis/terribilis-ludum \
     --mount type=bind,source=/media/data,target=/home/terribilis/data \
-           --mount type=bind,source=$HOME/projects/graeae,target=/home/bravo/graeae \
+    --mount type=bind,source=$HOME/projects/graeae,target=/home/terribilis/graeae \
     -it terribilis-pypy-official
